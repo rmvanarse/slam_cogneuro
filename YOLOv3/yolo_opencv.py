a107367d871a78f457efdd6373dde17c58a80440
Akhil Tarikere <atarikere@gmail.com>
2019-11-16 00:53:26 +0530

    YOLO for two images

--- YOLOv3/yolo_opencv.py
+++ YOLOv3/yolo_opencv.py
@@ -10,8 +10,10 @@ import argparse
 import numpy as np
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True,
-                help = 'path to input image')
+ap.add_argument('-i1', '--image1', required=True,
+                help = 'path to input image1')
+ap.add_argument('-i2', '--image2', required=True,
+                help = 'path to input image2')
 ap.add_argument('-c', '--config', required=True,
                 help = 'path to yolo config file')
 ap.add_argument('-w', '--weights', required=True,
@@ -41,64 +43,76 @@ def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     
-image = cv2.imread(args.image)
+image1 = cv2.imread(args.image1)
+image2 = cv2.imread(args.image2)
+image = [image1, image2]
 
-Width = image.shape[1]
-Height = image.shape[0]
-scale = 0.00392
+listClasses = [[],[]]
 
-classes = None
+for i in range(len(image)):
+    Width = image[i].shape[1]
+    Height = image[i].shape[0]
+    # print(Width, Height)
+    scale = 0.00392
 
-with open(args.classes, 'r') as f:
-    classes = [line.strip() for line in f.readlines()]
+    classes = None
 
-COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    with open(args.classes, 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
 
-net = cv2.dnn.readNet(args.weights, args.config)
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
-blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+    net = cv2.dnn.readNet(args.weights, args.config)
 
-net.setInput(blob)
+    blob = cv2.dnn.blobFromImage(image[i], scale, (416,416), (0,0,0), True, crop=False)
 
-outs = net.forward(get_output_layers(net))
+    net.setInput(blob)
 
-class_ids = []
-confidences = []
-boxes = []
-conf_threshold = 0.5
-nms_threshold = 0.4
+    outs = net.forward(get_output_layers(net))
 
-
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            center_x = int(detection[0] * Width)
-            center_y = int(detection[1] * Height)
-            w = int(detection[2] * Width)
-            h = int(detection[3] * Height)
-            x = center_x - w / 2
-            y = center_y - h / 2
-            class_ids.append(class_id)
-            confidences.append(float(confidence))
-            boxes.append([x, y, w, h])
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
 
 
-indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.4:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+                print(center_x, center_y, class_id)
+                listClasses[i].append((center_x, center_y, class_id))
 
-for i in indices:
-    i = i[0]
-    box = boxes[i]
-    x = box[0]
-    y = box[1]
-    w = box[2]
-    h = box[3]
-    draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
 
-cv2.imshow("object detection", image)
-cv2.waitKey()
-    
-cv2.imwrite("object-detection.jpg", image)
-cv2.destroyAllWindows()
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for j in indices:
+        j = j[0]
+        box = boxes[j]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        draw_prediction(image[i], class_ids[j], confidences[j], round(x), round(y), round(x+w), round(y+h))
+
+    # cv2.imshow("object detection", image[i])
+    cv2.waitKey()
+        
+    cv2.imwrite("object-detection.jpg", image[i])
+    cv2.destroyAllWindows()
+    print('..')
+# print(boxes)
+
+print(listClasses)

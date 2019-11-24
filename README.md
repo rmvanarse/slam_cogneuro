30dddf3b71b0926d5ad902448b9eae8e39f26ffb
Akhil Tarikere <atarikere@gmail.com>
2019-11-25 00:54:01 +0530

    Added vector subtraction and depth component

--- README.md
+++ README.md
@@ -2,24 +2,34 @@
 Estimation of change in position using object tracking and a depth map
 
 ##TODO
+
 - [X] Get class locations from YOLO
 - [ ] Get depthmap from Kinect
 - [X] Algorithm for matching corresponding objects in different frames
 - [ ] Get Kinect data
-- [ ] Vector equations for position change
+- [X] Vector equations for position change
+- [ ] Scale factor for the vector subtraction
 
 ## Dependencies:
+
 * opencv
 * imageio?
 * pytorch
 
 ## Running
+
 YOLO (with two images as arguments):
 ```
-python3 yolo_opencv.py --image1 <image1>.jpg --image2 <image2>.jpg --config yolov3.cfg --weights yolov3.weights --classes yolov3.txt
+python3 yolo_opencv.py --image1 <image1>.jpg --image2 <image2>.jpg --depthImage1 <depth1>.pgm --depthImage2 <depth2>.pgm --config yolov3.cfg --weights yolov3.weights --classes yolov3.txt
+```
+Example:
+```
+python3 yolo_opencv.py --image1 r-1.ppm --image2 r-2.ppm --depthImage1 d-1.pgm --depthImage2 d-2.pgm --config yolov3.cfg --weights yolov3.weights --classes yolov3.txt
+
 ```
 
 ## Limitations
+
 * Revolution around a single object cannot be detected.
 * Motion in Z-axis will require multiple objects on both sides of the center to cancel out the X and Y axes errors.
 * Will have to be combined with other odometry methods.
--- YOLOv3/yolo_opencv.py
+++ YOLOv3/yolo_opencv.py
@@ -5,7 +5,7 @@ Topic:      Object detection plus localisation.
 Authors:    Akhil Tarikere, 
             Rishikesh Vanarse, 
             Pranav Guruprasad and
-            Sri Shreyas Sundaresan only.
+            Srisreyas Sundaresan only.
 
 Date:       (October, 2019)
 
@@ -18,9 +18,17 @@ import argparse
 import numpy as np
 
 
-# Defining the constants.
+# Defining the CONSTANTS.
+#The maximum distance in pixels we want to detect the same image in consecutive images.
 thresholdDistance = 500
+
+#The minimum probability we want YOLO to detect objects with.
 confidenceThreshold = 0.4
+
+#To normalise the images, this should be proportional to the depth.
+scaleFactor = 1 
+
+
 
 
 # Taking two images and the other inputs.
@@ -29,6 +37,10 @@ ap.add_argument('-i1', '--image1', required=True,
                 help = 'path to input image1')
 ap.add_argument('-i2', '--image2', required=True,
                 help = 'path to input image2')
+ap.add_argument('-d1', '--depthImage1', required=True,
+                help = 'path to input the depth image of image1')
+ap.add_argument('-d2', '--depthImage2', required=True,
+                help = 'path to input the depth image of image2')
 ap.add_argument('-c', '--config', required=True,
                 help = 'path to yolo config file')
 ap.add_argument('-w', '--weights', required=True,
@@ -38,6 +50,7 @@ ap.add_argument('-cl', '--classes', required=True,
 args = ap.parse_args()
 
 
+
 def get_output_layers(net):
     
     layer_names = net.getLayerNames()
@@ -45,6 +58,7 @@ def get_output_layers(net):
     output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
     return output_layers
+
 
 
 def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
@@ -59,11 +73,18 @@ def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
 
 
-# Getting the images and storing them in the array.
+# Getting the images and storing them in a list.
 image1 = cv2.imread(args.image1)
 image2 = cv2.imread(args.image2)
 image = [image1, image2]
 
+# Getting the depth images of the corresponding images.
+depthImage1 = cv2.imread(args.depthImage1, cv2.IMREAD_GRAYSCALE)
+depthImage2 = cv2.imread(args.depthImage2, cv2.IMREAD_GRAYSCALE)
+
+
+# To store the classes and the coordinates.
+# The first element is the first image, and the second image is the second element.
 listClasses = [[],[]]
 
 
@@ -137,6 +158,19 @@ for i in range(len(image)):
 print(listClasses)
 
 
+
+print("----------resultantVectors------------")
+
+
+# Dictionary to calculate the average.
+vectorAverage = {
+    'x' : 0,
+    'y' : 0,
+    'z' : 0,
+    'count' : 0
+}
+
+
 # To compare between the various classes in the two photos.
 # listClasses[0] = Photo-1, listClasses[1] = Photo-2.
 for sets1 in listClasses[0]:
@@ -148,4 +182,48 @@ for sets1 in listClasses[0]:
                 # Append the co-ordinates to a new list.
                 # Find the new position with respect to that object.
                 # Find the average position with respect all the objects.
-                print("Yaay2!", sets1, sets2)
+
+                # Defining two dictionaries to hold the values of the coordinates of the objects detected in the images.
+                vector1 = {}
+                vector2 = {}
+                resultantVector = {}
+
+                vector1['x'] = sets1[0]
+                vector1['y'] = sets1[1]
+                vector1['z'] = depthImage1[sets1[0]][sets1[1]]
+
+                vector2['x'] = sets2[0]
+                vector2['y'] = sets2[1]
+                vector2['z'] = depthImage2[sets2[0]][sets2[1]]
+
+                # Calculating the resultant vector from the subtraction.
+                resultantVector['x'] = vector2['x'] - vector1['x']
+                resultantVector['y'] = vector2['y'] - vector1['y']
+                resultantVector['z'] = vector2['z'] - vector1['z']
+                resultantVector['objectType'] = sets1[2]
+
+                print('x=', resultantVector['x'], ', y=', resultantVector['y'], ', z=', resultantVector['z'], ', objectType=',resultantVector['objectType'])
+
+                # Adding the values to the total sum to calculate the average.
+                vectorAverage['count'] += 1
+                vectorAverage['x'] = vectorAverage['x'] + resultantVector['x']
+                vectorAverage['y'] = vectorAverage['y'] + resultantVector['y']
+                vectorAverage['z'] = vectorAverage['z'] + resultantVector['z']
+
+
+print("")
+# Finally calculating the averages of the vector subtractions.
+try:
+    vectorAverage['x'] = vectorAverage['x'] / vectorAverage['count']
+    vectorAverage['y'] = vectorAverage['y'] / vectorAverage['count']
+    vectorAverage['z'] = vectorAverage['z'] / vectorAverage['count']
+
+# Catching the exception when the count might be 0.
+except ZeroDivisionError:
+    print("Count of pair of object detected is 0")
+
+finally:
+    print("----Final average of the vector subtractions is----")
+    print('xAverage=', vectorAverage['x'], ', yAverage=', vectorAverage['y'], ', zAverage=', vectorAverage['z'])
+
+

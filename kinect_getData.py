5ddbc2362b6a1275ed3e7df129b566a6282c63a3
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-11-27 18:23:03 +0530

    Added depth subscriber and callback

--- kinect_getData.py
+++ kinect_getData.py
@@ -15,7 +15,13 @@ from cv_bridge import CvBridge, CvBridgeError
 
 num_images = 3
 interval = 0.5
+
+#last_response is for RGB and last_response1 is for depth
 last_response = time.time()
+last_response1 = time.time()
+
+RGB_Topic = '/camera/rgb/image_color'
+Depth_Topic = '/camera/rgb/image_color'
 
 def callback_rgb(msg):
 	"""
@@ -33,8 +39,27 @@ def callback_rgb(msg):
 	if(time.time() - last_response > interval):
 		print("Success!")
 		cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
-		cv2.imwrite('KinectResults/Test.jpg', cv2_img)
+		cv2.imwrite('KinectResults/RGB.jpg', cv2_img)
 		last_response = time.time()
+
+def callback_depth(msg):
+	"""
+	Arguments:
+		msg: The message from the topic
+
+	Returns: None
+
+	Saves the latest published RGB image after every interval
+	
+
+	"""
+	#print("Callback successful!")
+	global last_response1
+	if(time.time() - last_response1 > interval):
+		print("Success!")
+		cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+		cv2.imwrite('KinectResults/Depth.jpg', cv2_img)
+		last_response1 = time.time()
 
 
 
@@ -44,7 +69,10 @@ rospy.init_node('Vision')
 bridge = CvBridge()
 
 
-sub_rgb = rospy.Subscriber('/camera/rgb/image_color', Image, callback_rgb)
+sub_rgb = rospy.Subscriber(RGB_Topic, Image, callback_rgb)
+
+#Import required msg type and change from Image
+sub_depth = rospy.Subscriber(Depth_Topic, Image, callback_depth)
 
 rospy.spin()
 
